c8f46ae13f82ae55598d3a8544dd7eca9d9be917
Jesus M <marquez.jay444@gmail.com>
2023-05-31 17:41:29 -0500

    finishing touches

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
+import path from 'path'
+import url from 'url'
 import express from 'express'
 import * as dotenv from 'dotenv'
 import cors from 'cors'
+import mime from 'mime'
 
 import connectDB from './database/connect.js'
 import postRoutes from './routes/postRoutes.js'
@@ -8,15 +11,31 @@ import dalleRoutes from './routes/dalleRoutes.js'
 
 dotenv.config()
 
+const __filename = url.fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 const app = express()
 app.use(cors())
-app.use(express.json({ limit: '66mb' }))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/v1/post', postRoutes)
 app.use('/api/v1/dalle', dalleRoutes)
+app.use('/assets', express.static('public', {
+    setHeaders: (res, path) => {
+        if (mime.getType(path) === 'text/css') {
+            res.setHeader('Content-Type', 'text/css');
+        }
+    }
+}));
+
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '..', 'client', 'dist')))
+}
 
 app.get('/', async (req, res) => {
-    res.send('Hello from DALL-E!')
+    res.sendFile(path.join(__dirname, '..', 'client', 'dist', 'index.html'))
 })
 
 const startServer = async () => {
